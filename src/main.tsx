58a9da2d8a26c8e25582f689423e0d2a6fb7d3cf
Kaymmi Nunes <kaymmiknb@gmail.com>
2026-07-19 10:31:21 -0300

    refactor:

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    'O elemento com o identificador "root" não foi encontrado.',
+  );
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <App />
