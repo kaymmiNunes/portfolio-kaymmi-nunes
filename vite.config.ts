d449f14de138912a6df7a602f5ea36c7bf8c8fa6
Kaymmi Nunes <kaymmiknb@gmail.com>
2026-07-19 10:59:18 -0300

    fix: define diretório de build para o Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: "/",
+
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+
   server: {
     open: true,
+  },
+
+  preview: {
+    port: 4173,
   },
 });
